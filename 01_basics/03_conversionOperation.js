29dbf3604714a99fd733ed6bf18f88dba49592eb
imunnasingh <150954830+imunnasingh@users.noreply.github.com>
2024-12-09 11:54:17 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//"33"=> 33
+//"33aaj"=>NaN
+//true=>1; false=>0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=>true; 0=>false
+//""=>false
+//"Munna Singh"=>true
