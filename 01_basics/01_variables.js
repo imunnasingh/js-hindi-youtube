ecb7bc0761eabe1218d6d7a7875bc4a08ddbef67
imunnasingh <150954830+imunnasingh@users.noreply.github.com>
2024-12-09 11:29:36 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId =1837387
+let accountEmail = "jguib@gmail.com"
+var accounPassword = "73637"
+accountCity = "Delhi"
+
+
+
+// accountId =2 not allowed
+accountEmail ="jakdjs@gmail.com"
+accountPassword ="36474"
+accountCity ="Noida"
+let accountState
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accounPassword,accountCity.accountState])
