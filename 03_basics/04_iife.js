3a1eaa5738b0f352671feb1df21de58b8c2894ef
imunnasingh <150954830+imunnasingh@users.noreply.github.com>
2024-12-11 09:28:42 +0000

    push code

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// immediately invoked function Expressions
+// named iief
+// (function chai(){
+//     console.log('DB CONNECTED');
+// })();
+
+
+((name) => {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('munna');
