6596a21bc5fef3e511d83006c3dca7e1511923e5
imunnasingh <150954830+imunnasingh@users.noreply.github.com>
2024-12-11 05:24:13 +0000

    push code

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -28,4 +28,37 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage("Munna"));
+// console.log(loginUserMessage("Munna"));
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    username: "munna",
+    price:199
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject(
+    {
+        username: "munna",
+        price: 399
+    }
+)
+
+const myNewArray = [200, 400, 100, 600]
+
+    function returnSecondValue(getArray){
+        return getArray[1]
+    }
+
+    // console.log(returnSecondValue(myNewArray));
+    console.log(returnSecondValue([200, 500, 749, 398]));
