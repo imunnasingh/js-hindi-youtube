c71ca233719fc0451c345053bdb0829a7cdd2700
imunnasingh <150954830+imunnasingh@users.noreply.github.com>
2024-12-11 10:06:52 +0000

    code push

---
+++ 04_control_flow/one.js
@@ -0,0 +1,17 @@
+//if
+
+const isUserLoggedIn = true
+
+if(3!=2){
+console.log("executed");
+}
+// <,>,<=,>=,==,!=,===
+
+const score =200
+
+if(score>100){
+    let power = "fly"
+    console.log(`User power${power}`);
+}
+
+console.log(`User power${power}`);
