3d61db946094ae4f73c76f03012cdaaa9d4aa5b7
imunnasingh <150954830+imunnasingh@users.noreply.github.com>
2024-12-12 04:48:37 +0000

    code push

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,5 +5,23 @@ const myObject = {
     swift: 'swift by apple'
 }
 for (const key in myObject){
-    console.log(myObject[key]);
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+        
+    }
+
+    // const map = new Map()
+    // map.set('IN', "India")
+    // map.set('USA', "India")
+    // map.set('Fr', "India")
+    // map.set('IN', "India")
+
+    // for (const key in map) {
+    //     console.log(key);
+        
+    // }
